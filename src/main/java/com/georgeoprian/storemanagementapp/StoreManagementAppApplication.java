b50c386c27143cd74534f5e2a6ddb94711a716b7
GeorgeOprian <george.oprian@pss.ro>
2025-09-01 20:19:05 +0300

    The project was initated

---
+++ src/main/java/com/georgeoprian/storemanagementapp/StoreManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.georgeoprian.storemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/georgeoprian/storemanagementapp/StoreManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeoprian.storemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
