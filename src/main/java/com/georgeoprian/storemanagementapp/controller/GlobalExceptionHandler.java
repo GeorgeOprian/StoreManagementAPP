1dd3211ee587ecac257213d4c2ac697e118f55f5
GeorgeOprian <george.oprian@pss.ro>
2025-09-02 16:10:09 +0300

    implement logging on ControllerAdvice and set the logging level on the application

--- src/main/java/com/georgeoprian/storemanagementapp/controller/GlobalExceptionHandler.java
+++ src/main/java/com/georgeoprian/storemanagementapp/controller/GlobalExceptionHandler.java
@@ -1,6 +1,12 @@
 package com.georgeoprian.storemanagementapp.controller;
 
 import com.georgeoprian.storemanagementapp.exception.ApiException;
+import com.georgeoprian.storemanagementapp.exception.BadRequestException;
+import com.georgeoprian.storemanagementapp.exception.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,8 +19,22 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<Map<String, Object>> handleAPIException(ApiException ex) {
+
+        log.debug("An exception was generated");
+
+        if (ex instanceof NotFoundException) {
+            log.error("Not Found: {}", ex.getMessage());
+        } else if (ex instanceof BadRequestException) {
+            log.error("Bad Request: {}", ex.getMessage());
+        } else {
+            log.error("Unrecognized ApiException: {}", ex.getMessage());
+        }
+
+
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("status", ex.getStatus());
@@ -22,6 +42,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         body.put("internalCode", ex.getCode());
 
         return ResponseEntity.status(ex.getStatus()).body(body);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneric(Exception ex, HttpServletRequest request) {
+
+        log.error("Unexpected error: ", ex);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        body.put("message", ex.getMessage());
+        body.put("URI", request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
     }
 
 }
